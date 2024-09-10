506c264e0882fdfd797a7d1e5987e5ce2be5b655
Samanthapintor <samanthap329@nycstudents.net>
2024-09-10 17:26:00 -0400

    lab 1

---
+++ Pintor_Samantha_Lab_1.cpp
@@ -0,0 +1,26 @@
+//Samantha Pintor
+//9/10/24
+//Lab One
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout <<"##########################################################################################" << endl;
+    cout <<"#                                                                                        #" << endl;
+    cout <<"#                                                                                        #" << endl;
+    cout <<"# Ways to access the Task Manager on your Windows computer:                              #" << endl;
+    cout <<"#                                                                                        #" << endl;
+    cout <<"#            Press the key combination Ctrl Plus Shift Plus Escape                       #" << endl;
+    cout <<"#                                                                                        #" << endl;
+    cout <<"#            Press the key combination Ctrl Plus Alt Plus Delete and select Task Manager #" << endl;
+    cout <<"#                                                                                        #" << endl;
+    cout <<"#            Task Manager in the Windows Start Menu search                               #" << endl;
+    cout <<"#                                                                                        #" << endl;
+    cout <<"#                                                                                        #" << endl;
+    cout <<"##########################################################################################" << endl; 
+    
+    return 0;
+}
